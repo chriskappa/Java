6f68329c78d7b97b4a8ae0cc7b9ce37fcc82fd83
chriskappa <chriskagkelis@yahoo.com>
2021-10-07 21:08:24 +0100

    First Commit]

---
+++ src/ClientHandler.java
@@ -0,0 +1,108 @@
+
+
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.nio.Buffer;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+
+
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    public Socket socket;
+    public BufferedReader bufferedReader;
+    public BufferedWriter bufferedWriter;
+    private String userName;
+
+
+
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())); // Getting output Streamer
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream())); // Getting input Streamer
+            this.userName = bufferedReader.readLine(); // Getting Users Username
+            clientHandlers.add(this); // Adding the user to the arraylist
+            broadcastMessage("SERVER: "+userName+" has entered the chat!");
+        }
+        catch(IOException e) {
+            closeConnections(socket,bufferedReader,bufferedWriter);
+        }
+    }
+
+
+
+
+    @Override
+    public void run() {
+
+        String messageFromClient;
+        // We are connected to client
+        while(socket.isConnected()) {
+            try {
+                messageFromClient = bufferedReader.readLine();
+                broadcastMessage(messageFromClient);
+
+            }
+            catch(IOException e) {
+                closeConnections(socket,bufferedReader,bufferedWriter);
+                break;
+            }
+
+        }
+
+    }
+
+    public void broadcastMessage(String messageToSend) {
+        for(ClientHandler clientHandler : clientHandlers) {
+            try {
+                // Checking if the current client is not the same person who sends the message
+                if(!clientHandler.userName.equals(userName) ) {
+                    System.out.println("Sending message to "+clientHandler.userName);
+                    clientHandler.bufferedWriter.write(messageToSend);
+                    clientHandler.bufferedWriter.newLine(); // Getting to new line and buffer is not waiting for any new data
+                    clientHandler.bufferedWriter.flush(); // Flushiing buffer before it gets full
+                }
+            }
+            catch(IOException e) {
+                closeConnections(socket,bufferedReader,bufferedWriter);
+            }
+
+        }
+    }
+
+
+    public void removeClientHanlder() {
+        clientHandlers.remove(this);
+        broadcastMessage("Server: test user Has Left The Chat!");
+
+    }
+
+    public void closeConnections(Socket socket,BufferedReader bufferedReader,BufferedWriter bufferedWriter) {
+        removeClientHanlder(); // Removing ClientHandler
+        try {
+            // Closing BufferedReader
+            if(bufferedReader != null) {
+                bufferedReader.close();
+            }
+            // Closing BufferedWriter
+            if(bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            // Closing Socket
+            if(socket != null) {
+                socket.close();
+            }
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/Server.java
@@ -0,0 +1,47 @@
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+public class Server {
+
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void runServer() {
+        try {
+//			Checcking while its not closed
+            while(!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("A new user has connected!");
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler); // Creating Thread
+                thread.start(); // Starting the Thread
+            }
+
+        }
+        catch(IOException e ) {
+            System.out.println("Error"+e);
+        }
+
+    }
+
+    public void closeServer() {
+        try {
+            if(serverSocket != null) {
+                serverSocket.close();
+            }
+        }
+        catch(IOException e) {
+            System.out.println("Error"+e);
+        }
+    }
+
+    public static void main (String[] args) throws IOException{
+        ServerSocket serverSocket = new ServerSocket(8818);
+        Server server = new Server(serverSocket);
+        server.runServer();
+    }
+}
